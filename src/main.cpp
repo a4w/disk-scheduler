37f9d8d8e57e68bb7a395e0b7ead6546d42dfb63
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-14 06:49:46 +0200

    FCFS test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 
+#include "FCFS.h"
+
 int main(int argc, char *argv[]){
+    FCFS fs;
+    fs.addRequest(DiskRequest(98));
+    fs.addRequest(DiskRequest(183));
+    fs.addRequest(DiskRequest(37));
+    fs.addRequest(DiskRequest(122));
+    fs.addRequest(DiskRequest(14));
+    fs.addRequest(DiskRequest(124));
+    fs.addRequest(DiskRequest(65));
+    fs.addRequest(DiskRequest(67));
+
+    fs.setHeadPosition(53);
+    std::cout << fs.getHeadMovements() << std::endl;
     return 0;
 }
